1fe7f7b838a9c7c9590e8f67ae7ac103dc448159
thinethwic <134825739+thinethwic@users.noreply.github.com>
2026-03-24 09:26:48 +0530

    Add Department to Location

--- src/main/java/com/cic/inventory/dtos/DepartmentDTO.java
+++ src/main/java/com/cic/inventory/dtos/DepartmentDTO.java
@@ -1,6 +1,7 @@
 package com.cic.inventory.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -13,4 +14,13 @@ public class DepartmentDTO {
     @NotBlank(message = "Department Code is required")
     @Size(min = 6, max = 50, message = "Department Code must be between 6 and 50 characters")
     private String code;
+
+    @NotNull(message = "Location is required")
+    private LocationRef location;
+
+    @Data
+    public static class LocationRef {
+        @NotNull(message = "Location ID is required")
+        private Long id;
+    }
 }
--- src/main/java/com/cic/inventory/dtos/LocationDTO.java
+++ src/main/java/com/cic/inventory/dtos/LocationDTO.java
@@ -12,9 +12,6 @@ public class LocationDTO {
     @NotBlank(message = "Name is required")
     private String name;
 
-    @NotNull(message = "Location ID cannot be null")
-    private Location location;
-
     @NotBlank(message = "Location Code is required")
     @Size(min = 6, max = 50, message = "Location Code must be between 6 and 50 characters")
     private String code;
