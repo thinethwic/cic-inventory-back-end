b8339994b3abd7cd88148a11d3047f4e03dadc61
thinethwic <134825739+thinethwic@users.noreply.github.com>
2026-03-27 10:40:45 +0530

    Error with time zone

--- src/main/java/com/cic/inventory/InventoryApplication.java
+++ src/main/java/com/cic/inventory/InventoryApplication.java
@@ -3,10 +3,13 @@ package com.cic.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class InventoryApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Colombo"));
 		SpringApplication.run(InventoryApplication.class, args);
 	}
 
